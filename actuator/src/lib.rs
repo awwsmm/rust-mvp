a6c42e7552233a959a13f080a91ecf3fa30b0f88
Andrew William Watson <aww@awwsmm.com>
2024-01-03 16:44:52 -0500

    move actuator_temperature get_handler() to actuator default_handler()

--- actuator/src/lib.rs
+++ actuator/src/lib.rs
@@ -1,5 +1,6 @@
+use std::collections::HashMap;
 use std::fmt::Display;
-use std::net::IpAddr;
+use std::net::{IpAddr, TcpStream};
 use std::sync::{Arc, Mutex};
 use std::thread::JoinHandle;
 
@@ -7,8 +8,9 @@ use mdns_sd::{ServiceDaemon, ServiceInfo};
 
 use device::address::Address;
 use device::id::Id;
+use device::message::Message;
 use device::name::Name;
-use device::Device;
+use device::{Device, Handler};
 
 /// An Actuator mutates the Environment.
 pub trait Actuator: Device {
@@ -16,7 +18,64 @@ pub trait Actuator: Device {
 
     fn get_environment(&self) -> &Arc<Mutex<Option<ServiceInfo>>>;
 
-    fn get_environment_info(&self) -> Option<ServiceInfo>;
+    /// By default, an `Actuator` forwards all incoming requests to the `Environment`.
+    fn default_handler(&self) -> Handler {
+        // Anything which depends on self must be cloned outside of the |stream| lambda.
+        // We cannot refer to `self` inside of this lambda.
+        let self_name = self.get_name().clone();
+        let self_id = self.get_id().clone();
+        let self_model = Self::get_model();
+
+        let environment = Arc::clone(self.get_environment());
+
+        Box::new(move |stream| {
+            if let Ok(message) = Message::read(stream) {
+                if message.start_line == "POST /command HTTP/1.1" {
+                    // send a Command to this Actuator (Command is in the body)
+                    //     ex: curl 10.12.50.26:5454/command -d '{"name":"HeatTo","value":"25"}'
+
+                    // match message.body.as_ref().map(Command::parse) {
+                    //     Some(Ok(command)) => {
+                    let environment = environment.lock().unwrap();
+
+                    match environment.as_ref().map(Self::extract_address) {
+                        Some(address) => {
+                            println!("[Actuator] forwarding body {:?} as-is to environment @ {}", message.body, address);
+
+                            let mut environment = TcpStream::connect(address.to_string()).unwrap();
+
+                            let mut headers = HashMap::new();
+                            headers.insert("id", self_id.to_string());
+                            headers.insert("model", self_model.to_string());
+
+                            // forward Command to Environment
+                            let forwarded_command = message.with_headers(headers);
+                            forwarded_command.write(&mut environment);
+
+                            // ack request from Controller to close the socket
+                            let ack = Message::ack();
+                            ack.write(stream)
+                        }
+                        None => {
+                            let msg = "could not find environment";
+                            Self::handler_failure(self_name.clone(), stream, msg)
+                        }
+                    }
+                    // }
+                    //     _ => {
+                    //         let msg = format!("cannot parse body as command: {:?}", message.body);
+                    //         Self::handler_failure(self_name.clone(), stream, msg.as_str())
+                    //     }
+                    // }
+                } else {
+                    let msg = format!("cannot parse request: {}", message.start_line);
+                    Self::handler_failure(self_name.clone(), stream, msg.as_str())
+                }
+            } else {
+                Self::handler_failure(self_name.clone(), stream, "unable to read Message from stream")
+            }
+        })
+    }
 
     fn start(ip: IpAddr, port: u16, id: Id, name: Name, group: String) -> JoinHandle<()> {
         std::thread::spawn(move || {
--- actuator_temperature/src/lib.rs
+++ actuator_temperature/src/lib.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-use std::net::TcpStream;
 use std::sync::{Arc, Mutex};
 
 use mdns_sd::ServiceInfo;
@@ -7,12 +5,9 @@ use mdns_sd::ServiceInfo;
 use actuator::Actuator;
 use device::address::Address;
 use device::id::Id;
-use device::message::Message;
 use device::model::Model;
 use device::name::Name;
 use device::{Device, Handler};
-
-use crate::command::Command;
 
 pub mod command;
 
@@ -40,63 +35,8 @@ impl Device for TemperatureActuator {
         self.address
     }
 
-    /// By default, an `Actuator` forwards all incoming requests to the `Environment`.
     fn get_handler(&self) -> Handler {
-        // Anything which depends on self must be cloned outside of the |stream| lambda.
-        // We cannot refer to `self` inside of this lambda.
-        let self_name = self.get_name().clone();
-        let self_id = self.get_id().clone();
-        let self_model = Self::get_model();
-
-        let environment = Arc::clone(self.get_environment());
-
-        Box::new(move |stream| {
-            if let Ok(message) = Message::read(stream) {
-                if message.start_line == "POST /command HTTP/1.1" {
-                    // send a Command to this Actuator (Command is in the body)
-                    //     ex: curl 10.12.50.26:5454/command -d '{"name":"HeatTo","value":"25"}'
-
-                    match message.body.as_ref().map(Command::parse) {
-                        Some(Ok(command)) => {
-                            let environment = environment.lock().unwrap();
-
-                            match environment.as_ref().map(Self::extract_address) {
-                                Some(address) => {
-                                    println!("[Actuator] forwarding command {} to environment @ {}", command, address);
-
-                                    let mut environment = TcpStream::connect(address.to_string()).unwrap();
-
-                                    let mut headers = HashMap::new();
-                                    headers.insert("id", self_id.to_string());
-                                    headers.insert("model", self_model.to_string());
-
-                                    // forward Command to Environment
-                                    let forwarded_command = message.with_headers(headers);
-                                    forwarded_command.write(&mut environment);
-
-                                    // ack request from Controller to close the socket
-                                    let ack = Message::ack();
-                                    ack.write(stream)
-                                }
-                                None => {
-                                    let msg = "could not find environment";
-                                    Self::handler_failure(self_name.clone(), stream, msg)
-                                }
-                            }
-                        }
-                        _ => {
-                            let msg = format!("cannot parse body as command: {:?}", message.body);
-                            Self::handler_failure(self_name.clone(), stream, msg.as_str())
-                        }
-                    }
-                } else {
-                    let msg = format!("cannot parse request: {}", message.start_line);
-                    Self::handler_failure(self_name.clone(), stream, msg.as_str())
-                }
-            } else {
-                Self::handler_failure(self_name.clone(), stream, "unable to read Message from stream")
-            }
-        })
+        self.default_handler()
     }
 }
 
@@ -112,11 +52,5 @@ impl Actuator for TemperatureActuator {
 
     fn get_environment(&self) -> &Arc<Mutex<Option<ServiceInfo>>> {
         &self.environment
-    }
-
-    fn get_environment_info(&self) -> Option<ServiceInfo> {
-        let lock = self.environment.lock();
-        let guard = lock.unwrap();
-        guard.clone()
     }
 }
