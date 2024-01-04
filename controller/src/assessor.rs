697a804c5b6fe84b08ae5e35858fe29dfc2ba7a8
Andrew William Watson <aww@awwsmm.com>
2024-01-04 17:24:28 -0500

    100% coverage of Assessors

--- controller/src/assessor.rs
+++ controller/src/assessor.rs
@@ -1,5 +1,6 @@
 use phf::{phf_map, Map};
 
+use actuator_temperature::command::Command as Thermo5000;
 use datum::unit::Unit;
 use datum::Datum;
 
@@ -19,11 +20,44 @@ pub static DEFAULT_ASSESSOR: Map<&str, Assessor> = phf_map! {
         assert_eq!(datum.unit, Unit::DegreesC);
 
         if t > 28.0 {
-            Some(Box::new(actuator_temperature::command::Command::CoolBy(t - 25.0)))
+            Some(Box::new(Thermo5000::CoolBy(t - 25.0)))
         } else if t < 22.0 {
-            Some(Box::new(actuator_temperature::command::Command::HeatBy(25.0 - t)))
+            Some(Box::new(Thermo5000::HeatBy(25.0 - t)))
         } else {
             None
         }
     }}
 };
+
+#[cfg(test)]
+mod assessor_tests {
+    use chrono::Utc;
+
+    use super::*;
+
+    #[test]
+    fn test_thermo5000() {
+        let assessor = DEFAULT_ASSESSOR.get("thermo5000").unwrap();
+
+        let too_cold = Datum::new(21.0, Unit::DegreesC, Utc::now());
+
+        let actual = (assessor.assess)(&too_cold).unwrap();
+        let expected = Thermo5000::HeatBy(4.0);
+
+        // it is very difficult to compare a Box<dyn actuator::Command> to a Thermo5000::Command
+        // in lieu of directly comparing them, compare their serialized forms
+
+        assert_eq!(actual.to_string(), expected.to_string());
+
+        let too_hot = Datum::new(30.0, Unit::DegreesC, Utc::now());
+        let actual = (assessor.assess)(&too_hot).unwrap();
+        let expected = Thermo5000::CoolBy(5.0);
+
+        assert_eq!(actual.to_string(), expected.to_string());
+
+        let just_right = Datum::new(25.0, Unit::DegreesC, Utc::now());
+        let actual = (assessor.assess)(&just_right);
+
+        assert!(actual.is_none());
+    }
+}
